e94c192945acb3b7662beb002cbe5fe7d080e595
inzy164 <mdinzimam06351@gmail.com>
2026-09-01 18:05:45 +0530

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{margin:0, fontFamily:'system-ui', background:'#0f0f0f'}}>{children}</body>
-    </html>
-  )
-}
